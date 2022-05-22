383bda78ab2c300cff1b99504a72f665b8b5fd26
RaphaelMarto <92374387+RaphaelMarto@users.noreply.github.com>
2022-05-22 21:19:58 +0200

    correct de la page profil

--- Frontend/src/assets/Composants/getProfil.js
+++ Frontend/src/assets/Composants/getProfil.js
@@ -4,45 +4,55 @@ import imageProfile from '../images/default.png';
 import '../CSS/Navbar.css';
 import authHeader from "../services/auth-header";
 
-async function GetProfil(){
-    let Name = 'Louis';
-    const [currentShow, setShow] = useState(false);
-    const [currentShow2, setShow2] = useState(false);
-    const [currentShow3, setShow3] = useState(false);
-    //const [Profil, setProfil] = useState([]);
-    let ProfilUser
-    let Profil
-
+let ProfilUser = [];
+async function recup() {
     await axios
         .get("http://localhost:8080/User/test", {headers: authHeader()})
         .then(
             res => ProfilUser = res.data);
-    if (JSON.parse(localStorage.getItem("user")) !== null){
-        if (ProfilUser.length !== 0){
-            console.log(ProfilUser);
-            await axios
+    console.log(ProfilUser);
+    return ProfilUser;
+}
+recup();
+
+function GetProfil(){
+    const [currentShow, setShow] = useState(false);
+    const [currentShow2, setShow2] = useState(false);
+    const [currentShow3, setShow3] = useState(false);
+
+
+    const [Profil, setProfil] = useState([]);
+    useEffect(() => {
+        axios
             .get("http://localhost:8080/User/pseudo/" + ProfilUser.Nickname)
             .then(
-                res => Profil = res.data.data
-            );
+                res => setProfil(res.data.data));
+    }, []);
+
+    console.log(Profil);
+    console.log(ProfilUser);
+    if (JSON.parse(localStorage.getItem("user")) !== null){
+        if (ProfilUser.length !== 0){
             if (Profil.length !== 0){
                 console.log(Profil);
                 return (<div>
                 <img src={imageProfile} alt={'ProfilPicture'} height={'250px'} width={'250px'}></img>
-                {console.log(Profil)}
                 <form>
                     <label>Nom : {Profil[0].Nickname}</label> <button onClick={(e) => {e.preventDefault(); setShow(currentShow => !currentShow)}}>Modifier</button> <br/>
                     { currentShow ? <div><label name={'newName'} >Nouveau nom</label><input name={'newName'} type={'text'}></input></div> : null }
                     <label>E-mail : {Profil[0].Mail}</label> <button onClick={(e) => {e.preventDefault(); setShow2(currentShow2 => !currentShow2)}}>Modifier</button><br/>
                     { currentShow2 ? <div><label name={'newMail'} >Nouveau Mail</label><input name={'newMail'} type={'text'}></input></div> : null }
                     <label>mdp : ********</label> <button onClick={(e) => {e.preventDefault(); setShow3(currentShow3 => !currentShow3)}}>Modifier</button><br/>
-                    { currentShow3 ? <div><label name={'newMdp'} >Nouveau mdp</label><input name={'newMdp'} type={'text'}></input></div> : null }
+                    { currentShow3 ? <div><label name={'newMdp'} >Nouveau mdp</label><input name={'newMdp'} type={'password'}></input></div> : null }
                 </form>
             </div>);
             }
             else {
                 return (<div>Loading</div>)
             }
+        }
+        else {
+            return (<div>Loading</div>)
         }
 
     }
