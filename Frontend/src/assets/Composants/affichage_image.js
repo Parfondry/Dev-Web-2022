2e5d4d8ea840a0116824e84293eeebd8822a8fb1
NathanGregoar <he201832@students.ephec.be>
2022-05-25 20:10:17 +0200

    Ajout du post d'un nouveau commentaire
    Ajout du post d'un nouveau commentaire (récupère l'id de l'utilisateur connecté actuellement mais idImage entré manuellement)

--- Frontend/src/assets/Composants/affichage_image.js
+++ Frontend/src/assets/Composants/affichage_image.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import Comments from '../Composants/getComment';
+import PostComment from "./post_comment";
 
 function AffichageImage(){
     const [Images, setImages] = useState([]);
@@ -18,6 +19,10 @@ function AffichageImage(){
                 <img id='image' src={Image.File} className="image-logo" alt="image" />
                 <div id='commentaires'>
                     <h5>Commentaires</h5>
+                    <fieldset>
+                        <legend>Poster un commentaire</legend>
+                        <PostComment/>  
+                    </fieldset>
                     <fieldset>
                      <legend>Pseudo utilisateur</legend>
                         <Comments/>
