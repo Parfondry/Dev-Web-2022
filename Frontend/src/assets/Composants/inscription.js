6aaefec2a6597cfeff403728262fb14b15a50dbc
Louis Lombaert <l.lombaert@students.ephec.be>
2022-05-26 17:52:39 +0200

    test sur inscription

--- Frontend/src/assets/Composants/inscription.js
+++ Frontend/src/assets/Composants/inscription.js
@@ -3,38 +3,56 @@ import '../CSS/Contenu.css';
 import axios from "axios";
 import { useNavigate } from 'react-router';
 
-    async function HandleSubmitGet(){
-        let Users
-        await axios
-            .get("http://localhost:8080/user")
-            .then(res => Users = res.data.data);
-        return (<div>{Users.length !=0 && Users.map(user => <li id={user.id} key={user.id}>{user.nickname}</li>)}</div>);
-    }
 
+    function verify(pseudo, mdp, mail, birth){
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,4}))$/;
+        if (pseudo == "" || mdp == "" || mail == "" || birth == ""){
+          alert("all the input must be comleted !");
+          return false;
+        }
+        else if (pseudo.length < 3){
+            alert("the pseudo must be at least 3 caracters long !");
+            return false;
+        }
+        else if (mdp.length < 3){
+          alert("the pseudo must be at least 3 caracters long !");
+          return false;
+        }
+        else if(! re.test(mail)){
+          alert("the email address is not correct");
+          return false 
+        }
+        else{
+            return true;
+        }
+      }
     function InscriptioncC(){
       let navigate = useNavigate();
+      
       function handleSubmit(e) {
-          e.preventDefault();
-          var self = this;
+        e.preventDefault();
+        var self = this;
+        if (verify(e.target['pseudo'].value, e.target['mdp'].value, e.target['Mail'].value, e.target['Birth'].value)){
 
-          fetch('http://localhost:8080/user', {
-              method: 'POST',
-              headers: {'Content-type': 'application/json'},
-              body: JSON.stringify({nickname:e.target['pseudo'].value,PWD: e.target['mdp'].value,
-                  Mail:e.target['Mail'].value,Birth:e.target['Birth'].value})
-          })
-              .then(response => response.json())
-              .then(function(body){
-                  if (body.accessToken) {
-                      localStorage.setItem("user", JSON.stringify(body));
-                  }
-                  navigate('/');
-              });
-      }
-    return(
-        <div>
-            <form id="Inscription" onSubmit={handleSubmit}>
-                <fieldset>
+            fetch('http://localhost:8080/user', {
+                method: 'POST',
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify({nickname:e.target['pseudo'].value,PWD: e.target['mdp'].value,
+                    Mail:e.target['Mail'].value,Birth:e.target['Birth'].value})
+            })
+                .then(response => response.json())
+                .then(function(body){
+                    if (body.accessToken) {
+                        localStorage.setItem("user", JSON.stringify(body));
+                    }
+                    navigate('/');
+                });
+        }
+    }
+        return(
+            <div>
+                <form id="Inscription" onSubmit={handleSubmit}>
+                    <fieldset>
                     <legend>Inscription</legend>
                     <label>Pseudo: </label>
                     <input type="text" name="pseudo" placeholder='Pseudo'></input><br></br>
@@ -52,3 +70,4 @@ import { useNavigate } from 'react-router';
   }
 
 export default InscriptioncC;
+export {verify};
--- Frontend/src/assets/Composants/inscription.test.js
+++ Frontend/src/assets/Composants/inscription.test.js
@@ -1,23 +1,61 @@
-import React from "react"
-import { screen, render } from "@testing-library/react";
-//import "@testing-library/jest-dom";
-import InscriptionC from "./inscription"
+import React from "react";
+import '@testing-library/jest-dom'
 
-describe("InscriptionForm", () => {
-    test("should render the basic fields", () => {
-      render(<InscriptionC />);
-      /*expect(screen.getByRole("textbox", { name: /pseudo/i })).toBeInTheDocument();
-      expect(screen.getByRole("textbox", { name: /mdp/i })).toBeInTheDocument();
-      expect(screen.getByRole("textbox", { name: /Mail/i })).toBeInTheDocument();
-      expect(screen.getByRole("textbox", { name: /Birth/i })).toBeInTheDocument();
-      expect(
-        screen.getByRole("spinbutton", { name: /servings/i })
-      ).toBeInTheDocument();
-      expect(
-        screen.getByRole("button", { name: /add ingredient/i })
-      ).toBeInTheDocument();
-      expect(screen.getByRole("button", { name: /save/i })).toBeInTheDocument();*/
-    });
-  });
+//import verify from "./inscription"
+const inscr = require ('./inscription')
 
+test("chek valid inscription", () => {
+  expect(inscr.verify("Louis", "password_secret", "louis@student.com", "11/03/2001")).toBe(true)
+});
 
+test("chek unvalid inscription with an empty password", () => {
+  expect(inscr.verify("Louis", "", "louis@student.com", "11/03/2001")).toBe(false)
+});
+
+test("chek unvalid inscription with an empty pseudo", () => {
+  expect(inscr.verify("", "password_secret", "louis@student.com", "11/03/2001")).toBe(false)
+});
+
+test("chek unvalid inscription with an empty mail", () => {
+  expect(inscr.verify("Louis", "password_secret", "", "11/03/2001")).toBe(false)
+});
+
+test("chek unvalid inscription with an empty birth", () => {
+  expect(inscr.verify("louis", "password_secret", "louis@student.com", "")).toBe(false)
+});
+
+test("chek unvalid inscription with a too short pseudo", () => {
+  expect(inscr.verify("ui", "password_secret", "louis@student.com", "11/12/2003")).toBe(false)
+});
+
+test("chek unvalid inscription with a too short password", () => {
+  expect(inscr.verify("louis", "pa", "louis@student.com", "12/15/2006")).toBe(false)
+});
+
+test("chek unvalid inscription with a too short pseudo and password", () => {
+  expect(inscr.verify("ui", "pa", "louis@student.com", "11/03/2002")).toBe(false)
+});
+
+test("chek unvalid inscription with an invalid email", () => {
+  expect(inscr.verify("louis", "password_secret", "@student.com", "12/05/2004")).toBe(false)
+});
+
+test("chek unvalid inscription with an invalid email", () => {
+  expect(inscr.verify("louis", "password_secret", "louis@student.comoeoe", "12/05/2004")).toBe(false)
+});
+
+test("chek unvalid inscription with an invalid email", () => {
+  expect(inscr.verify("louis", "password_secret", "louis@", "12/05/2004")).toBe(false)
+});
+
+test("chek unvalid inscription with an invalid email", () => {
+  expect(inscr.verify("louis", "password_secret", "louis@student.c", "12/05/2004")).toBe(false)
+});
+
+test("chek unvalid inscription with an invalid email", () => {
+  expect(inscr.verify("louis", "password_secret", "louis", "12/05/2004")).toBe(false)
+});
+
+test("chek unvalid inscription with an invalid email", () => {
+  expect(inscr.verify("louis", "password_secret", "@", "12/05/2004")).toBe(false)
+});
